784c1899dc9062f4eaaf81f1b73f010a4e106a4d
Farihul Rouf <farihulrouf@gmail.com>
2022-11-03 16:08:32 +0700

    plangsung

--- opdb.js
+++ opdb.js
@@ -95,47 +95,111 @@ function showCreateAnggaran(anggaran) {
       }
     })
   }
+  else if(header_title=="Tender") {
+    Swal.fire({
+      title:  anggaran,
+      html:
+        '<input id="id" type="hidden">' +
+        '<select id="Ultra" class="swal2-input" onchange="alert(this.value)"> <option value="0">Select</option><option value="8">text1</option> <option value="5">text2</option></select>'+
+        '<input id="paket" class="swal2-input"  placeholder="Paket">' +
+        '<input id="pagu" class="swal2-input"  placeholder="Pagu">'+
+        '<input type="date" class="swal2-input" id="pemilihan" name="trip-start" value="2018-07-22" min="2018-01-01" max="2025-12-31">'+
+        '<input type="date" class="swal2-input" id="pelaksanaan" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31">'+
+        '<input type="date" class="swal2-input" id="pemanfaatan" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31">'+
+        '<input id="pdn" class="swal2-input" placeholder="PDN %">',
+
+      focusConfirm: false,
+      preConfirm: () => {
+
+        CreateDetailPagu(api_url_tender, header_title)
+      
+      }
+    })
+  }
   else {
     Swal.fire({
       title:  anggaran,
       html:
         '<input id="id" type="hidden">' +
-        '<input id="name" class="swal2-input" placeholder="Name">' +
-        '<input id="paket"  class="swal2-input" placeholder="Paket">' +
-        '<input id="pagu" class="swal2-input" placeholder="Pagu">',
+        '<select id="Ultra" class="swal2-input" onchange="alert(this.value)"> <option value="0">Select</option><option value="8">text1</option> <option value="5">text2</option></select>'+
+        '<input id="paket" class="swal2-input"  placeholder="Paket">' +
+        '<input id="pagu" class="swal2-input"  placeholder="Pagu">'+
+        '<input type="date" class="swal2-input" id="pelaksanaan" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31">'+
+        '<input id="pdn" class="swal2-input" placeholder="PDN %">',
+
       focusConfirm: false,
       preConfirm: () => {
-        if (header_title == 'Tender') {
-          //const api_param = api_url_tender
-          CreateDetailPagu(api_url_tender, header_title)
-          
+
+        if(header_title == 'Langsung') {
+          console.log("anggaran langsung di eksekusi")
+          CreateDetailLain(api_url_langsung, header_title)
         }
-        else if(header_title == 'Langsung') {
+        else if(header_title ==='Kecuali'){
+          CreateDetailLain(api_url_pengecualian, header_title)
+        }
+      
+      }
+    })
+    /*
+     if(header_title == 'Langsung') {
           //const api_param = api_url_langsung
-          CreateDetailPagu(api_url_langsung, header_title)
         }
         else if(header_title=="Kecuali") {
           //const api_param = api_url_pengecualian
-          CreateDetailPagu(api_url_pengecualian, header_title)
         }
-      }
-    })
+      */
+
   }
  
 }
 
+const  CreateTenderCepat = () =>{
+  
+}
+
 
 function CreateDetailPagu(api_param, header_title) {
-  const name = document.getElementById("name").value;
+
+  const waktupemilihan = document.getElementById("pemilihan").value;
+  const waktupelaksanaan = document.getElementById("pelaksanaan").value;
+  const waktupemanfaatan = document.getElementById("pemanfaatan").value;
+  const pdn = document.getElementById("pdn").value;
   const paket = document.getElementById("paket").value;
   const pagu = document.getElementById("pagu").value;
+  //console.log(pagu)
+  //console.log(pdn)
+  //console.log(waktupemilihan)
+  //console.log(waktupelaksanaan)
+  //console.log(waktupemilihan)
+ // console.log(pagu)
+  //console.log(paket)
+  console.log(id_global)
+  //console.log(pagu)
+  
   console.log("ini hereader",header_title)
   const xhttp = new XMLHttpRequest();
   xhttp.open("POST", api_param);
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify({ 
-    "name": name, "paket": paket, "pagu": pagu, "jadwal": "12-12-2022", "idpagu": id_global
+    "name": "Penelitian pengembangan Dan perekayasaan",
+    "paket": paket,
+    "pagu": pagu,
+    "jadwal": waktupemanfaatan,
+    "pemilihan": waktupemilihan,
+    "pelaksanaan": waktupelaksanaan,
+    "pdn": pdn,
+    "idpagu": id_global
   }));
+/*
+  "name" : "Penelitian pengembangan Dan perekayasaan",
+  "paket" : "silver",
+  "pagu": "10000000",
+  "jadwal": "12-12-2022",
+  "pelaksanaan": "12-12-2022",
+  "pemilihan": "12-12-12",
+  "pdn": "10",
+  "idpagu": "635c8575a49fb441d6ff4670"
+  */
   xhttp.onreadystatechange = function() {
     if(header_title=="Langsung") {
       detailLangsung(id_global)
@@ -151,9 +215,49 @@ function CreateDetailPagu(api_param, header_title) {
       console.log("ini di eksekusi")
     }
   };
+  
   //console.log(id_global)
 }
 
+function CreateDetailLain(api_param, header_title) {
+
+  //const waktupemilihan = document.getElementById("pemilihan").value;
+  //const waktupelaksanaan = document.getElementById("pelaksanaan").value;
+  const waktupemanfaatan = document.getElementById("pelaksanaan").value;
+  const pdn = document.getElementById("pdn").value;
+  const paket = document.getElementById("paket").value;
+  const pagu = document.getElementById("pagu").value;
+  //console.log(pagu)
+ 
+  console.log(id_global)
+  //console.log(pagu)
+  
+  console.log("ini hereader",header_title)
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", api_param);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({ 
+    "name": "Penelitian pengembangan Dan perekayasaan",
+    "paket": paket,
+    "pagu": pagu,
+    "jadwal": waktupemanfaatan,
+    "pdn": pdn,
+    "idpagu": id_global
+  }));
+
+  xhttp.onreadystatechange = function() {
+    if(header_title=="Langsung") {
+      detailLangsung(id_global)
+    }
+   
+    else if(header_title="Kecuali"){
+      detailKecuali(id_global)
+      console.log("ini di eksekusi")
+    }
+  };
+  
+  //console.log(id_global)
+}
 
 
 function paguDelete(id) {
@@ -197,6 +301,7 @@ function detailPage(id) {
     y.style.display = "none";
     x.style.display = "block";
     id_global = id;
+    detailGolbalAnggaran()
     detailPaguItem(id)
     detailAnggaran(id)
     detailTender(id)
@@ -216,26 +321,27 @@ function detailPaguItem(id){
         //console.log(objects.data.data)
         const posts = objects.data.data
         var node = ''
-        console.log(posts);
+        var divHtml = '' 
+        //console.log(posts);
         node = document.createTextNode(posts.name);
+        
         for(i=0;i<3;i++){
 
-        const para = document.createElement("p");
-        if(i==0){
-          node = document.createTextNode(posts.name);
-        }
-        else if (i==1) {
-          node = document.createTextNode(posts.paguopdp);
-        }
-        else if(i==2) {
-          node = document.createTextNode(posts.paguorp);
-        }
-          para.appendChild(node);
-          const element = document.getElementById("infodetail");
-          element.appendChild(para);
-        }
-        
-        
+          const para = document.createElement("p");
+          if(i==0){
+            node = document.createTextNode(posts.name);
+          }
+          else if (i==1) {
+            node = document.createTextNode(posts.paguopdp);
+          }
+          else if(i==2) {
+            node = document.createTextNode(posts.paguorp);
+          }
+            para.appendChild(node);
+            const element = document.getElementById("infodetail");
+            element.appendChild(para);
+          }
+           
       }
     };
 }
@@ -272,10 +378,48 @@ function detailAnggaran(id) {
             trHTML += '<td>'+1+'</td>';
             trHTML += '<td>'+object['name']+'</td>';
             trHTML += '<td>'+object['pagu']+'</td>';
+            trHTML += '<td>'+object['pagu']+'</td>';
             trHTML += '<td><a href="#"><span class="material-symbols-outlined" onclick="showUserEditBox(\''+id_obj+'\')">edit </span></a>';
             trHTML += '<a href="#"><span class="material-symbols-outlined" onclick="detailDelete(\''+id_obj+'\',`anggaran`,\''+api_url_anggaran+'\')">delete_forever</span></a></td>';
            
             trHTML += "</tr>";
+          }
+        }
+        
+        document.getElementById("anggaran").innerHTML = trHTML;
+        
+      }
+    };
+    //console.log("data di load sudah");
+}
+
+function detailGolbalAnggaran() {
+  //console.log(id)
+  const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", api_url_anggaran);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {      
+        var trHTML = ''; 
+        const objects = JSON.parse(this.responseText);
+        if(objects.data.data === null) {
+          console.log('data kosong')
+        }
+        else {
+          for (let object of objects.data.data) {
+          
+            let id_obj = object['id']
+            console.log(objects.data.data)
+            trHTML += '<tr>'; 
+            trHTML += '<td>'+1+'</td>';
+            trHTML += '<td>'+object['name']+'</td>';
+            trHTML += '<td>'+object['pagu']+'</td>';
+            trHTML += '<td>'+object['pagu']+'</td>';
+            trHTML += '<td><a href="#"><span class="material-symbols-outlined" onclick="showUserEditBox(\''+id_obj+'\')">edit </span></a>';
+            trHTML += '<a href="#"><span class="material-symbols-outlined" onclick="detailDelete(\''+id_obj+'\',`anggaran`,\''+api_url_anggaran+'\')">delete_forever</span></a></td>';
+           
+            trHTML += "</tr>";
+          
           }
         }
         
@@ -307,6 +451,10 @@ function detailTender(id) {
           trHTML += '<td>'+object['name']+'</td>';
           trHTML += '<td>'+object['paket']+'</td>';
           trHTML += '<td>'+object['pagu']+'</td>';
+          trHTML += '<td>'+object['pemilihan']+'</td>';
+          trHTML += '<td>'+object['pelaksanaan']+'</td>'; 
+          trHTML += '<td>'+object['jadwal']+'</td>';
+          trHTML += '<td>'+object['pdn']+'</td>'; 
           trHTML += '<td><a href="#"><span class="material-symbols-outlined" onclick="showUserEditBox(\''+id_obj+'\')">edit </span></a>';
           trHTML += '<a href="#"><span class="material-symbols-outlined" onclick="detailDelete(\''+id_obj+'\',`tender`,\''+api_url_tender+'\')">delete_forever</span></a></td>';
         
@@ -343,6 +491,8 @@ function detailLangsung(id) {
           trHTML += '<td>'+object['name']+'</td>';
           trHTML += '<td>'+object['paket']+'</td>';
           trHTML += '<td>'+object['pagu']+'</td>';
+          trHTML += '<td>'+object['jadwal']+'</td>';
+          trHTML += '<td>'+object['pdn']+'</td>';
           trHTML += '<td><a href="#"><span class="material-symbols-outlined" onclick="showUserEditBox(\''+id_obj+'\')">edit </span></a>';
           trHTML += '<a href="#"><span class="material-symbols-outlined" onclick="detailDelete(\''+id_obj+'\', `langsung`,\''+api_url_langsung+'\')">delete_forever</span></a></td>';
         
@@ -374,6 +524,8 @@ function detailKecuali(id) {
           trHTML += '<tr>'; 
           trHTML += '<td>'+1+'</td>';
           trHTML += '<td>'+object['name']+'</td>';
+          trHTML += '<td>'+object['paket']+'</td>';
+          trHTML += '<td>'+object['pagu']+'</td>';
           trHTML += '<td>'+object['paket']+'</td>';
           trHTML += '<td>'+object['pagu']+'</td>';
           trHTML += '<td><a href="#"><span class="material-symbols-outlined" onclick="showUserEditBox(\''+id_obj+'\')">edit </span></a>';
@@ -437,22 +589,21 @@ function paguEdit() {
     }));
     xhttp.onreadystatechange = function() {
       loadTable();
-      /*if (this.readyState == 4 && this.status == 200) {
-        const objects = JSON.parse(this.responseText);
-        Swal.fire(objects['message']);
-        loadTable();
-        console.log("eksekusi")
-      }*/
+    
     };
 }
 
 function showPagu(){
-
+    //const element = document.getElementsByTagName('p')
+   //const element = document.getElementById("infodetail");
+    // element.remove()
     var x = document.getElementById("detail");
     var y = document.getElementById("opdb")
     x.style.display = "none";
     y.style.display = "block";
+    
 }
+
   
 
 
